c99f9241aead0c390a190612eb5d93b8c719f5a0
Binh Nguyen <binhnx286@gmail.com>
2024-09-17 11:18:26 +0700

    Add home

--- BookStore/BookStore/urls.py
+++ BookStore/BookStore/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Welcome to the Django application!")
 urlpatterns = [
+    path('', home),
     path("admin/", admin.site.urls),
     path("api/", include("user.urls")),
 ]
