ad610b4ec358d8b93caa3e29c7b3a12c00a24817
Binh Nguyen <binhnx286@gmail.com>
2024-09-24 15:43:51 +0700

    debug true

--- BookStore/BookStore/settings.py
+++ BookStore/BookStore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f=d^)bi3zzju(@r8o8dw4_jt75zkm!qf(z@ka98w(jmv-xor!^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['*']
