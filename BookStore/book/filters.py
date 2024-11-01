b49a2706191f496ed1f0b370dfb04f2356759bfa
Binh Nguyen <binhnx286@gmail.com>
2024-11-01 19:52:31 +0700

    Update filter for product muti value

--- BookStore/book/filters.py
+++ BookStore/book/filters.py
@@ -3,9 +3,9 @@ from django_filters import rest_framework as filters
 from .models import Product
 
 class ProductFilter(filters.FilterSet):
-    publication_year = filters.CharFilter(method='filter_publication_year')
-    publisher = filters.CharFilter(field_name="publisher", lookup_expr="icontains")
-    sub_category = filters.CharFilter(field_name="sub_category__name", lookup_expr="exact")
+    publication_year = filters.CharFilter(method='filter_multiple_values', field_name="publication_year")
+    publisher = filters.CharFilter(method='filter_multiple_values', field_name="publisher")
+    sub_category = filters.CharFilter(method='filter_multiple_values', field_name="sub_category__name")
     new_price_min = filters.NumberFilter(field_name="new_price", lookup_expr="gte")
     new_price_max = filters.NumberFilter(field_name="new_price", lookup_expr="lte")
 
@@ -13,6 +13,7 @@ class ProductFilter(filters.FilterSet):
         model = Product
         fields = ['publication_year', 'publisher', 'sub_category', 'new_price_min', 'new_price_max']
 
-    def filter_publication_year(self, queryset, name, value):
-        years = value.split(",")  # Tách các năm bằng dấu phẩy
-        return queryset.filter(publication_year__in=years)
+    def filter_multiple_values(self, queryset, name, value):
+        values = value.split(",")  # Tách các giá trị bằng dấu phẩy
+       
+        return queryset.filter(**{f"{name}__in": values})
