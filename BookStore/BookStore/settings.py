59b0cc942212d24681b6b3e10ba0bfdfcbde9cdb
Binh Nguyen <binhnx286@gmail.com>
2024-11-06 10:49:25 +0700

    CORS_ALLOW_ALL_ORIGINS = True

--- BookStore/BookStore/settings.py
+++ BookStore/BookStore/settings.py
@@ -27,8 +27,10 @@ DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['*']
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://fe-book-store.vercel.app",
+]
 CORS_ALLOW_METHODS = [
     'DELETE',
     'GET',
